e2137dc270a3943970dc919296d20e8072033f40
Shashichandra K <55590583+shashichandrak@users.noreply.github.com>
2024-06-27 16:26:17 +0530

    Azure Service Bus Message Testing

---
+++ ServiceBusMessaging.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace AzureServiceBusMessaging
+{
+    partial class ServiceBusMessaging
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServiceBusMessaging));
+            lblServiceBusNameSpace = new Label();
+            txtServiceBusNamespace = new TextBox();
+            grpbxServiceBusNameSpace = new GroupBox();
+            txtAsbMessage = new TextBox();
+            label3 = new Label();
+            txtasbTopicName = new TextBox();
+            label2 = new Label();
+            txtASBConnectionString = new TextBox();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            btnBulkUpdate = new Button();
+            grpbsSessionDetails = new GroupBox();
+            btnSendMessages = new Button();
+            txtSessionid2 = new TextBox();
+            lblSessionId2 = new Label();
+            txtSessionid1 = new TextBox();
+            lblSessionId1 = new Label();
+            txtasbNamespaceKeyName = new TextBox();
+            label1 = new Label();
+            grpbxServiceBusNameSpace.SuspendLayout();
+            groupBox2.SuspendLayout();
+            grpbsSessionDetails.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblServiceBusNameSpace
+            // 
+            lblServiceBusNameSpace.AutoSize = true;
+            lblServiceBusNameSpace.Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblServiceBusNameSpace.Location = new Point(20, 190);
+            lblServiceBusNameSpace.Margin = new Padding(4, 0, 4, 0);
+            lblServiceBusNameSpace.Name = "lblServiceBusNameSpace";
+            lblServiceBusNameSpace.Size = new Size(199, 24);
+            lblServiceBusNameSpace.TabIndex = 1;
+            lblServiceBusNameSpace.Text = "Servicebus Namespace";
+            // 
+            // txtServiceBusNamespace
+            // 
+            txtServiceBusNamespace.Enabled = false;
+            txtServiceBusNamespace.Location = new Point(306, 190);
+            txtServiceBusNamespace.Margin = new Padding(4, 5, 4, 5);
+            txtServiceBusNamespace.Name = "txtServiceBusNamespace";
+            txtServiceBusNamespace.ReadOnly = true;
+            txtServiceBusNamespace.Size = new Size(610, 37);
+            txtServiceBusNamespace.TabIndex = 2;
+            // 
+            // grpbxServiceBusNameSpace
+            // 
+            grpbxServiceBusNameSpace.Controls.Add(txtAsbMessage);
+            grpbxServiceBusNameSpace.Controls.Add(label3);
+            grpbxServiceBusNameSpace.Controls.Add(txtasbTopicName);
+            grpbxServiceBusNameSpace.Controls.Add(label2);
+            grpbxServiceBusNameSpace.Controls.Add(txtASBConnectionString);
+            grpbxServiceBusNameSpace.Controls.Add(label4);
+            grpbxServiceBusNameSpace.Controls.Add(groupBox2);
+            grpbxServiceBusNameSpace.Controls.Add(grpbsSessionDetails);
+            grpbxServiceBusNameSpace.Controls.Add(txtasbNamespaceKeyName);
+            grpbxServiceBusNameSpace.Controls.Add(label1);
+            grpbxServiceBusNameSpace.Controls.Add(txtServiceBusNamespace);
+            grpbxServiceBusNameSpace.Controls.Add(lblServiceBusNameSpace);
+            grpbxServiceBusNameSpace.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            grpbxServiceBusNameSpace.Location = new Point(13, 2);
+            grpbxServiceBusNameSpace.Margin = new Padding(4, 5, 4, 5);
+            grpbxServiceBusNameSpace.Name = "grpbxServiceBusNameSpace";
+            grpbxServiceBusNameSpace.Padding = new Padding(4, 5, 4, 5);
+            grpbxServiceBusNameSpace.Size = new Size(970, 1453);
+            grpbxServiceBusNameSpace.TabIndex = 3;
+            grpbxServiceBusNameSpace.TabStop = false;
+            grpbxServiceBusNameSpace.Text = "Service Bus Details";
+            // 
+            // txtAsbMessage
+            // 
+            txtAsbMessage.Location = new Point(306, 398);
+            txtAsbMessage.Margin = new Padding(4, 5, 4, 5);
+            txtAsbMessage.Multiline = true;
+            txtAsbMessage.Name = "txtAsbMessage";
+            txtAsbMessage.Size = new Size(610, 407);
+            txtAsbMessage.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(18, 398);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(175, 24);
+            label3.TabIndex = 12;
+            label3.Text = "Servicebus Message";
+            // 
+            // txtasbTopicName
+            // 
+            txtasbTopicName.Location = new Point(306, 317);
+            txtasbTopicName.Margin = new Padding(4, 5, 4, 5);
+            txtasbTopicName.Name = "txtasbTopicName";
+            txtasbTopicName.Size = new Size(610, 37);
+            txtasbTopicName.TabIndex = 11;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(20, 317);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(200, 24);
+            label2.TabIndex = 10;
+            label2.Text = "Servicebus Topic Name";
+            // 
+            // txtASBConnectionString
+            // 
+            txtASBConnectionString.Location = new Point(306, 61);
+            txtASBConnectionString.Margin = new Padding(4, 5, 4, 5);
+            txtASBConnectionString.Multiline = true;
+            txtASBConnectionString.Name = "txtASBConnectionString";
+            txtASBConnectionString.Size = new Size(610, 119);
+            txtASBConnectionString.TabIndex = 9;
+            txtASBConnectionString.Leave += txtASBConnectionString_Leave;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(18, 61);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(251, 24);
+            label4.TabIndex = 8;
+            label4.Text = "Servicebus Connection String";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnBulkUpdate);
+            groupBox2.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox2.Location = new Point(20, 1103);
+            groupBox2.Margin = new Padding(4, 5, 4, 5);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(4, 5, 4, 5);
+            groupBox2.Size = new Size(899, 238);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "BulkUpdate with Session";
+            // 
+            // btnBulkUpdate
+            // 
+            btnBulkUpdate.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnBulkUpdate.Location = new Point(236, 83);
+            btnBulkUpdate.Margin = new Padding(4, 5, 4, 5);
+            btnBulkUpdate.Name = "btnBulkUpdate";
+            btnBulkUpdate.Size = new Size(459, 55);
+            btnBulkUpdate.TabIndex = 4;
+            btnBulkUpdate.Text = "Send Bulk Update Message";
+            btnBulkUpdate.UseVisualStyleBackColor = true;
+            btnBulkUpdate.Click += btnBulkUpdate_Click;
+            // 
+            // grpbsSessionDetails
+            // 
+            grpbsSessionDetails.Controls.Add(btnSendMessages);
+            grpbsSessionDetails.Controls.Add(txtSessionid2);
+            grpbsSessionDetails.Controls.Add(lblSessionId2);
+            grpbsSessionDetails.Controls.Add(txtSessionid1);
+            grpbsSessionDetails.Controls.Add(lblSessionId1);
+            grpbsSessionDetails.Location = new Point(20, 815);
+            grpbsSessionDetails.Margin = new Padding(4, 5, 4, 5);
+            grpbsSessionDetails.Name = "grpbsSessionDetails";
+            grpbsSessionDetails.Padding = new Padding(4, 5, 4, 5);
+            grpbsSessionDetails.Size = new Size(899, 238);
+            grpbsSessionDetails.TabIndex = 5;
+            grpbsSessionDetails.TabStop = false;
+            grpbsSessionDetails.Text = "Session Details ";
+            // 
+            // btnSendMessages
+            // 
+            btnSendMessages.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSendMessages.Location = new Point(236, 173);
+            btnSendMessages.Margin = new Padding(4, 5, 4, 5);
+            btnSendMessages.Name = "btnSendMessages";
+            btnSendMessages.Size = new Size(459, 55);
+            btnSendMessages.TabIndex = 4;
+            btnSendMessages.Text = "Send 5 Messages per Session";
+            btnSendMessages.UseVisualStyleBackColor = true;
+            btnSendMessages.Click += btnSendMessages_Click;
+            // 
+            // txtSessionid2
+            // 
+            txtSessionid2.Location = new Point(186, 118);
+            txtSessionid2.Margin = new Padding(4, 5, 4, 5);
+            txtSessionid2.Name = "txtSessionid2";
+            txtSessionid2.Size = new Size(191, 37);
+            txtSessionid2.TabIndex = 3;
+            // 
+            // lblSessionId2
+            // 
+            lblSessionId2.AutoSize = true;
+            lblSessionId2.Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSessionId2.Location = new Point(21, 118);
+            lblSessionId2.Margin = new Padding(4, 0, 4, 0);
+            lblSessionId2.Name = "lblSessionId2";
+            lblSessionId2.Size = new Size(108, 24);
+            lblSessionId2.TabIndex = 2;
+            lblSessionId2.Text = "Session Id 2";
+            // 
+            // txtSessionid1
+            // 
+            txtSessionid1.Location = new Point(186, 42);
+            txtSessionid1.Margin = new Padding(4, 5, 4, 5);
+            txtSessionid1.Name = "txtSessionid1";
+            txtSessionid1.Size = new Size(191, 37);
+            txtSessionid1.TabIndex = 1;
+            // 
+            // lblSessionId1
+            // 
+            lblSessionId1.AutoSize = true;
+            lblSessionId1.Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSessionId1.Location = new Point(21, 42);
+            lblSessionId1.Margin = new Padding(4, 0, 4, 0);
+            lblSessionId1.Name = "lblSessionId1";
+            lblSessionId1.Size = new Size(108, 24);
+            lblSessionId1.TabIndex = 0;
+            lblSessionId1.Text = "Session Id 1";
+            // 
+            // txtasbNamespaceKeyName
+            // 
+            txtasbNamespaceKeyName.Enabled = false;
+            txtasbNamespaceKeyName.Location = new Point(306, 250);
+            txtasbNamespaceKeyName.Margin = new Padding(4, 5, 4, 5);
+            txtasbNamespaceKeyName.Name = "txtasbNamespaceKeyName";
+            txtasbNamespaceKeyName.ReadOnly = true;
+            txtasbNamespaceKeyName.Size = new Size(610, 37);
+            txtasbNamespaceKeyName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(18, 250);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(280, 24);
+            label1.TabIndex = 3;
+            label1.Text = "Servicebus Namespace Keyname";
+            // 
+            // ServiceBusMessaging
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(1017, 1503);
+            Controls.Add(grpbxServiceBusNameSpace);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 5, 4, 5);
+            MinimizeBox = false;
+            Name = "ServiceBusMessaging";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Servicebus Message Testing";
+            grpbxServiceBusNameSpace.ResumeLayout(false);
+            grpbxServiceBusNameSpace.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            grpbsSessionDetails.ResumeLayout(false);
+            grpbsSessionDetails.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Label lblServiceBusNameSpace;
+        private TextBox txtServiceBusNamespace;
+        private GroupBox grpbxServiceBusNameSpace;
+        private GroupBox grpbsSessionDetails;
+        private TextBox txtSessionid2;
+        private Label lblSessionId2;
+        private TextBox txtSessionid1;
+        private Label lblSessionId1;
+        private TextBox txtasbNamespaceKeyName;
+        private Label label1;
+        private Button btnSendMessages;
+        private GroupBox groupBox2;
+        private Button btnBulkUpdate;
+        private TextBox txtASBConnectionString;
+        private Label label4;
+        private TextBox txtasbTopicName;
+        private Label label2;
+        private TextBox txtAsbMessage;
+        private Label label3;
+    }
+}
---
+++ ServiceBusMessaging.cs
@@ -0,0 +1,120 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System.Text;
+using Message = Microsoft.Azure.ServiceBus.Message;
+
+namespace AzureServiceBusMessaging
+{
+    public partial class ServiceBusMessaging : Form
+    {
+        string asbConnectionString = string.Empty;
+
+        string asbContactMessageTemplate = "{\r\n  \"EntityType\": \"Contact\",\r\n  \"EventType\": \"Cmc.Core.Eventing.SavedEvent, Cmc.Core\",\r\n  \"ArgumentData\": {\r\n    \"ValidationMessages\": [],\r\n    \"OriginalEntity\": null,\r\n    \"Context\": null,\r\n    \"CancelPipelineExecution\": false\r\n  },\r\n  \"EventTimeStamp\": \"2023-05-11T15:33:35.4241471Z\",\r\n  \"EntityData\": \"{\\r\\n  \\\"IsExcludedCrmIntegration\\\": false,\\r\\n  \\\"AddressTypeId\\\": 0,\\r\\n  \\\"StudentInquiryRequired\\\": true,\\r\\n  \\\"ExtraCurricularsList\\\": [],\\r\\n  \\\"ProgramsList\\\": [],\\r\\n  \\\"EnrolledProgramIdList\\\": [],\\r\\n  \\\"EthnicitiesList\\\": [],\\r\\n  \\\"LeadSourcesList\\\": [],\\r\\n  \\\"DeleteVeteranDetails\\\": false,\\r\\n  \\\"Vendors\\\": [],\\r\\n  \\\"StudentAddressAssociation\\\": 2,\\r\\n  \\\"NewAssignedAdmissionsRepId\\\": 0,\\r\\n  \\\"AssignedAdmissionsRepReassignedDate\\\": \\\"0001-01-01T00:00:00\\\",\\r\\n  \\\"StudentRelationshipAddress\\\": null,\\r\\n  \\\"LastFourSsn\\\": \\\"\\\",\\r\\n  \\\"CustomProperties\\\": {},\\r\\n  \\\"MultiValueCustomProperties\\\": {},\\r\\n  \\\"OriginalCustomPropertiesValues\\\": null,\\r\\n  \\\"OriginalMultiValueCustomPropertiesValues\\\": null,\\r\\n  \\\"GenderPronounList\\\": [],\\r\\n  \\\"OverridePrevEducAltPellValidation\\\": false,\\r\\n  \\\"Id\\\": 10,\\r\\n  \\\"AcgEligReasonCode\\\": null,\\r\\n  \\\"AgencyId\\\": 0,\\r\\n  \\\"AlienNumber\\\": \\\"\\\",\\r\\n  \\\"ArAccountStatus\\\": \\\"X\\\",\\r\\n  \\\"ArBalance\\\": 0.0,\\r\\n  \\\"ArNextTransactionNumber\\\": 0,\\r\\n  \\\"AssignedAdmissionsRepId\\\": 68,\\r\\n  \\\"AthleticIdentifier\\\": null,\\r\\n  \\\"BestTimeToContact\\\": \\\"Anytime\\\",\\r\\n  \\\"CampusId\\\": 6,\\r\\n  \\\"CitizenId\\\": 0,\\r\\n  \\\"City\\\": \\\"\\\",\\r\\n  \\\"CollegeId\\\": null,\\r\\n  \\\"CountryId\\\": 1,\\r\\n  \\\"CountyId\\\": 0,\\r\\n  \\\"CreatedDateTime\\\": \\\"2023-04-21T04:04:23\\\",\\r\\n  \\\"CumulativeGpa\\\": null,\\r\\n  \\\"CumulativeGpaPoints\\\": null,\\r\\n  \\\"CurrencyCodeId\\\": null,\\r\\n  \\\"CurrentLda\\\": null,\\r\\n  \\\"DataBlockIndicator\\\": false,\\r\\n  \\\"DateOfBirth\\\": null,\\r\\n  \\\"DbiModifiedDate\\\": null,\\r\\n  \\\"DefaultAddressCode\\\": null,\\r\\n  \\\"DefaultMasterStudentAddressId\\\": null,\\r\\n  \\\"DefaultStudentAddressId\\\": null,\\r\\n  \\\"Disabled\\\": \\\"X\\\",\\r\\n  \\\"DriverLicenseNumber\\\": \\\"\\\",\\r\\n  \\\"DriverLicenseState\\\": \\\"\\\",\\r\\n  \\\"EmailAddress\\\": \\\"asbdemo@asbdemo.com\\\",\\r\\n  \\\"EmployabilityAboutInfo\\\": null,\\r\\n  \\\"EmployerId\\\": 0,\\r\\n  \\\"EmploymentStatusId\\\": 0,\\r\\n  \\\"ExternalStudentIdentifier\\\": null,\\r\\n  \\\"ExtraCurricularActivityId\\\": 0,\\r\\n  \\\"FacebookUrl\\\": null,\\r\\n  \\\"FaGradPlusCounselingDate\\\": null,\\r\\n  \\\"FaRigorousHighSchoolProgramCodeId\\\": null,\\r\\n  \\\"FirstName\\\": \\\"ASB DEMO\\\",\\r\\n  \\\"GenderId\\\": 6,\\r\\n  \\\"GpaCredits\\\": null,\\r\\n  \\\"HispanicLatino\\\": null,\\r\\n  \\\"HsAcademicGpa\\\": null,\\r\\n  \\\"InstagramUrl\\\": null,\\r\\n  \\\"IsActive\\\": true,\\r\\n  \\\"IsAllowedBulkRegistrationByTrack\\\": null,\\r\\n  \\\"IsBadAddress\\\": false,\\r\\n  \\\"IsBadPhone\\\": false,\\r\\n  \\\"IsDdVeteran\\\": false,\\r\\n  \\\"IsEftDefaultForStipends\\\": false,\\r\\n  \\\"IsInDistrict\\\": false,\\r\\n  \\\"IsSscrError11Received\\\": false,\\r\\n  \\\"LastActivityDate\\\": null,\\r\\n  \\\"LastInterestDate\\\": null,\\r\\n  \\\"LastModifiedDateTime\\\": \\\"2023-05-11T11:33:34.8303895-04:00\\\",\\r\\n  \\\"LastModifiedUserId\\\": 56,\\r\\n  \\\"LastName\\\": \\\"Test 2\\\",\\r\\n  \\\"LastNameFirstFour\\\": \\\"Test\\\",\\r\\n  \\\"LastStatementBeginDate\\\": null,\\r\\n  \\\"LastStatementCloseDate\\\": null,\\r\\n  \\\"LastStatementDate\\\": null,\\r\\n  \\\"LeadDate\\\": \\\"2023-04-21T13:33:00\\\",\\r\\n  \\\"LeadSourceId\\\": 692,\\r\\n  \\\"LeadTypeId\\\": 0,\\r\\n  \\\"LinkedInUrl\\\": null,\\r\\n  \\\"MaidenName\\\": \\\"\\\",\\r\\n  \\\"MaritalStatusId\\\": 0,\\r\\n  \\\"Mi\\\": \\\"\\\",\\r\\n  \\\"MiddleName\\\": \\\"\\\",\\r\\n  \\\"MobilePhoneNumber\\\": \\\"\\\",\\r\\n  \\\"NationalityId\\\": 0,\\r\\n  \\\"NickName\\\": \\\"\\\",\\r\\n  \\\"NiStudent\\\": false,\\r\\n  \\\"Note\\\": \\\"\\\",\\r\\n  \\\"OriginalAssignedAdmissionsRepId\\\": 68,\\r\\n  \\\"OriginalExpectedStartDate\\\": null,\\r\\n  \\\"OriginalStartDate\\\": null,\\r\\n  \\\"OtherEmailAddress\\\": \\\"\\\",\\r\\n  \\\"OtherPhoneNumber\\\": null,\\r\\n  \\\"PersonId\\\": 157,\\r\\n  \\\"PhoneNumber\\\": \\\"\\\",\\r\\n  \\\"Pin\\\": null,\\r\\n  \\\"PostalCode\\\": \\\"\\\",\\r\\n  \\\"PostalCodeFirstThree\\\": \\\"   \\\",\\r\\n  \\\"PreferredContactType\\\": \\\"Ph\\\",\\r\\n  \\\"PreferredName\\\": null,\\r\\n  \\\"PreviousEducationGpa\\\": null,\\r\\n  \\\"PreviousEducationId\\\": 0,\\r\\n  \\\"ProgramGroupId\\\": 0,\\r\\n  \\\"ProgramId\\\": 0,\\r\\n  \\\"RawFirstName\\\": \\\"ASBDEMO\\\",\\r\\n  \\\"RawLastName\\\": \\\"Test\\\",\\r\\n  \\\"RawPhoneNumber\\\": \\\"\\\",\\r\\n  \\\"RowVersion\\\": \\\"AAAAAABiT/k=\\\",\\r\\n  \\\"SchoolStatusId\\\": 1,\\r\\n  \\\"ShiftId\\\": 0,\\r\\n  \\\"SmsServiceProviderId\\\": null,\\r\\n  \\\"SourceSystem\\\": \\\"C\\\",\\r\\n  \\\"Ssn\\\": \\\"\\\",\\r\\n  \\\"StartDate\\\": null,\\r\\n  \\\"State\\\": \\\"\\\",\\r\\n  \\\"StatementComment\\\": \\\"\\\",\\r\\n  \\\"StreetAddress\\\": \\\"\\\",\\r\\n  \\\"StreetAddress2\\\": null,\\r\\n  \\\"StudentFullName\\\": null,\\r\\n  \\\"StudentIdentifier\\\": null,\\r\\n  \\\"StudentNumber\\\": \\\"10\\\",\\r\\n  \\\"SubscribeToSms\\\": false,\\r\\n  \\\"Suffix\\\": null,\\r\\n  \\\"SuffixId\\\": null,\\r\\n  \\\"TitleId\\\": 0,\\r\\n  \\\"TwitterUrl\\\": null,\\r\\n  \\\"Veteran\\\": \\\"X\\\",\\r\\n  \\\"WorkPhoneNumber\\\": \\\"\\\",\\r\\n  \\\"WorkPhoneNumberExtension\\\": \\\"\\\",\\r\\n  \\\"OriginalState\\\": \\\"\\\",\\r\\n  \\\"SecureState\\\": \\\"\\\",\\r\\n  \\\"ExtendedProperties\\\": [],\\r\\n  \\\"EntityState\\\": 2\\r\\n}\",\r\n  \"UserId\": 0\r\n}";
+
+        public ServiceBusMessaging()
+        {
+            InitializeComponent();
+        }
+        private void btnSendMessages_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            if (string.IsNullOrEmpty(txtASBConnectionString.Text.Trim()))
+            {
+                MessageBox.Show("Please enter Service Bus Connection String");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtasbTopicName.Text.Trim()))
+            {
+
+                MessageBox.Show("Please enter Service Bus Topic Name");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtSessionid1.Text))
+            {
+                MessageBox.Show("Please enter Session 1 Id");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtSessionid2.Text))
+            {
+                MessageBox.Show("Please enter Session 2 Id");
+                return;
+            }
+
+            JObject asbMessageobj = JObject.Parse(asbContactMessageTemplate);
+
+            var asbMessageSender = new MessageSender(asbConnectionString, txtasbTopicName.Text.Trim());
+            var messageList = new List<Message>();
+            //ServiceBusClient serviceBusClient = new ServiceBusClient(asbConnectionString);
+            //var asbMessageSender = serviceBusClient.CreateSender("anth-studentsync-stu-sbt-01");
+            for (var i = 0; i < 10; i++)
+            {
+                Message message = new Message();
+                asbMessageobj["EntityData"] = string.IsNullOrEmpty(txtAsbMessage.Text) ? asbContactMessageTemplate : txtAsbMessage.Text;
+                var id = i < 5 ? txtSessionid1.Text : txtSessionid2.Text;
+                asbMessageobj["EntityType"] = "SampleObjectType";
+                message.SessionId = id;
+                message.Body = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(asbMessageobj));
+                messageList.Add(message);
+            }
+            asbMessageSender.SendAsync(messageList).Wait();
+            MessageBox.Show($"Message sent to topic: {txtasbTopicName} successfully ");
+            txtSessionid1.Clear();
+            txtSessionid2.Clear();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void btnBulkUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtASBConnectionString.Text.Trim()))
+            {
+                MessageBox.Show("Please enter Service Bus Connection String");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtasbTopicName.Text.Trim()))
+            {
+
+                MessageBox.Show("Please enter Service Bus Topic Name");
+                return;
+            }           
+
+            this.Cursor = Cursors.WaitCursor;
+            JObject asbMessageobj = new JObject();
+            asbMessageobj["EntityData"] = txtAsbMessage.Text.Trim();
+            asbMessageobj["EntityType"] = "SampleObjectType";
+            JObject asbBulkMessage = new JObject();
+
+            ServiceBusClient serviceBusClient = new ServiceBusClient(asbConnectionString);
+            var asbMessageSender = serviceBusClient.CreateSender(txtasbTopicName.Text.Trim());
+            var sessionId = new Random().Next();
+            var entityBulkUpdate = new JArray();
+            for (var i = 0; i < 10; i++)
+            {
+                var entity = asbBulkMessage;
+                entity[$"Message{i}"] = entity["EntityData"].ToString()+" Update " + sessionId + i.ToString();
+                entityBulkUpdate.Add(entity);
+            }
+            asbMessageobj["EntityData"] = entityBulkUpdate;
+            ServiceBusMessage message = new ServiceBusMessage();
+            message.SessionId = sessionId.ToString();
+            message.Body = BinaryData.FromBytes(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(asbMessageobj)));
+            asbMessageSender.SendMessageAsync(message).Wait();
+            MessageBox.Show($"Bulk Message sent to topic: {txtasbTopicName.Text} successfully ");
+            this.Cursor = Cursors.Default;
+        }
+
+        private void txtASBConnectionString_Leave(object sender, EventArgs e)
+        {
+            asbConnectionString = txtASBConnectionString.Text.Trim();
+            var asbConnectionStringArray = asbConnectionString.Split(';');
+            var asbNameSpace = asbConnectionStringArray[0].Split('/')[2];
+            txtServiceBusNamespace.Text = asbNameSpace;
+            txtasbNamespaceKeyName.Text = asbConnectionStringArray[1].Split('=')[1];
+        }
+    }
+}
